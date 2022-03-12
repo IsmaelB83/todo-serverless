6be127dfb0ced9e0bb8571fca9c2d4ea9f3568e1
Ismael Bernal <ismaelbernal83@gmail.com>
2022-03-12 07:16:08 +0100

    Adding todo completed

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,40 +1,65 @@
+// Node modules
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda'
 import 'source-map-support/register'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
+import * as AWS from 'aws-sdk';
+// Own modules
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 import { getUserId } from '../utils';
 //import { createTodo } from '../../businessLogic/todos'
-import * as AWS from 'aws-sdk';
+import { createLogger } from '../../utils/logger'
+
+const logger = createLogger('auth')
 
 // Constants
-const DB_TABLE: string = process.env.GROUPS_TABLE!;
+const DB_TABLE: string = process.env.TODOS_TABLE!;
 const DOC_CLIENT = new AWS.DynamoDB.DocumentClient();
 
+/**
+ * Handler function to create a todo
+ */
 export const handler = middy(
   async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => {
     
-    const newTodo: CreateTodoRequest = JSON.parse(event.body)
-
-    const params = {
-      TableName: DB_TABLE,
-      Item: {
-        todoId: context.awsRequestId,
-        userId: getUserId(event),
-        createAt: new Date().toISOString(),
-        ...newTodo
+    try {
+      // Todo object
+      const newTodo: CreateTodoRequest = JSON.parse(event.body)
+      // Dynamo command
+      const params = {
+        TableName: DB_TABLE,
+        Item: {
+          todoId: context.awsRequestId,
+          userId: getUserId(event),
+          createAt: new Date().toISOString(),
+          ...newTodo
+        }
       }
+      // Log
+      logger.info('Creating todo', JSON.stringify(params.Item))
+      // Put object
+      await DOC_CLIENT.put(params).promise()
+      // Return OK
+      return {
+        statusCode: 200,
+        headers: { 'Access-Control-Allow-Origin': '*' },
+        body: JSON.stringify({
+          item: params.Item
+        })
+      } 
+    } catch (e) {
+      // Log
+      logger.error('Error creating todo', { error: e.message })
+      // Return KO
+      return {
+        statusCode: 500,
+        headers: { 'Access-Control-Allow-Origin': '*' },
+        body: JSON.stringify({
+          result: e.message
+        })
+      } 
     }
     
-    await DOC_CLIENT.put(params).promise()
-    
-    return {
-      statusCode: 200,
-      headers: { 'Access-Control-Allow-Origin': '*' },
-      body: JSON.stringify({
-        item: params.Item
-      })
-    }
   }
 )
 
--- client/src/api/todos-api.ts
+++ client/src/api/todos-api.ts
@@ -14,7 +14,7 @@ export async function getTodos(idToken: string): Promise<Todo[]> {
     },
   })
   console.log('Todos:', response.data)
-  return response.data.items
+  return response.data.Items
 }
 
 export async function createTodo(
